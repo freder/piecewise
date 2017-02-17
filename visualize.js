e9ec7e661ea810db30bc88610c724b2110148db7
Frederic Brodbeck <freder.code@gmail.com>
2017-02-17 12:45:25 +0100

    moved visualization code to sub-module

--- visualize.js
+++ visualize.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const R = require('ramda');
 const Canvas = require('canvas');
 const piecewise = require('./');
+const { visualization } = piecewise;
 
 
 const piecewiseEasingFn = piecewise.easing([
@@ -41,57 +42,30 @@ const padding = 20;
 const height = 100;
 const width = 500;
 
-// const Image = Canvas.Image;
 const canvas = new Canvas(
 	(2 * padding) + width,
 	(2 * padding) + height
 )
 const ctx = canvas.getContext('2d');
 
-function plotFunction(ctx, fn, opts) {
-	const { step, lineColor, lineWidth } = R.merge(
-		{
-			step: 0.01,
-			lineColor: 'rgb(0, 0, 0)',
-			lineWidth: 1,
-		},
-		opts
-	);
 
-	ctx.strokeStyle = lineColor;
-	ctx.lineWidth = lineWidth;
-
-	let t = 0;
-	ctx.beginPath();
-	ctx.moveTo(padding, padding + height);
-	while (t <= 1) {
-		ctx.lineTo(
-			padding + (t * width),
-			padding + height - (fn(t) * height));
-		t += step;
-	}
-	ctx.stroke();
-}
-
-let opts;
-
-opts = {
+let opts = {
 	lineColor: 'rgb(0, 0, 0)',
 	lineWidth: 5,
 };
-plotFunction(ctx, finalFn, opts);
+visualization.plotFunction(ctx, opts, finalFn);
 
 opts = {
 	lineColor: 'rgb(200, 200, 200)',
 	lineWidth: 4,
 };
-plotFunction(ctx, piecewiseEasingFn, opts);
+visualization.plotFunction(ctx, opts, piecewiseEasingFn);
 
 opts = {
 	lineColor: 'rgb(255, 0, 0)',
 	lineWidth: 2,
 };
-plotFunction(ctx, piecewiseEnvelopeFn, opts);
+visualization.plotFunction(ctx, opts, piecewiseEnvelopeFn);
 
 fs.writeFileSync('out.png', canvas.toBuffer());
 
